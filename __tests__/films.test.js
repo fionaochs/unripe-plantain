af6b2d6d42da015b039a474b6536e530a3fcd765
Fiona Ochs <fiona.oaks@gmail.com>
2020-03-26 16:19:12 -0700

    films testing passing

--- __tests__/films.test.js
+++ __tests__/films.test.js
@@ -4,9 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-  it('creates a film', () => {
-    const studio = getStudio();
-    const actor = getActor();
+  it('creates a film', async() => {
+    const studio = await getStudio();
+    const actor = await getActor();
     return request(app)
       .post('/api/v1/films')
       .send({
@@ -21,13 +21,14 @@ describe('app routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          title: 'test film',
-          studioId: studio._id,
-          released: '1985',
-          cast: {
-            role: 'water boy',
-            actorId: actor._id
-          },
+          title: expect.any(String),
+          studioId: expect.any(String),
+          released: expect.any(Number),
+          cast: [{
+            _id: expect.any(String),
+            role: expect.any(String),
+            actorId: expect.any(String)
+          }],
           __v: 0
         });
       });
@@ -36,18 +37,18 @@ describe('app routes', () => {
   it('gets a film by id', async() => {
     const film = await getFilm();
     return request(app)
-      .get(`/api/v1/studios/${studio._id}`)
+      .get(`/api/v1/films/${film._id}`)
       .then(res => {
-        expect(res.body).toEqual(studio);
+        expect(res.body).toEqual(film);
       });
   });
 
-  it('gets all studios', async() => {
-    const studios = await getStudios();
+  it('gets all films', async() => {
+    const films = await getFilms();
     return request(app)
-      .get('/api/v1/studios')
+      .get('/api/v1/films')
       .then(res => {
-        expect(res.body).toEqual(studios);
+        expect(res.body).toEqual(films);
       });
   });
 });
